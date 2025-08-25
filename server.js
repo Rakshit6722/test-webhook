4537ffdbbf242b5bd0b39ae4ccd669e319900ef7
Rakshit Chauhan <rakshit.chauhan@magicsw.com>
2025-08-25 13:12:14 +0530

    test webhook 3

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.use(express.urlencoded())
 app.get("/",(_, res) => res.send("server running"))
 
 app.post("/webhook", (req, res)=> {
+
+    console.log(JSON.stringify(req.body))
     console.log("webhook received")
 })
 
