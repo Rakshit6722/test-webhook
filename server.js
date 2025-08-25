9f7c0eb984bb85b6069f4b2460b43a04fbee4e25
Rakshit Chauhan <rakshit.chauhan@magicsw.com>
2025-08-25 13:05:42 +0530

    test webhooks

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.urlencoded())
+
+app.get("/",(_, res) => res.send("server running"))
+
+app.post("/webhook", (req, res)=> {
+    console.log("webhook received")
+})
+
+
+app.listen(3000, () => {
+    console.log("Server started")
+})
